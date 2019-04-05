3400df58d40b2e21a9a0a4b1b6d7a1a343a8a3ec
Steve Masterman <steve@vermonster.com>
2019-04-05 15:19:50 -0400

    wip

---
+++ src/lib.rs
@@ -0,0 +1,47 @@
+use std::fs;
+use std::error::Error;
+
+pub struct Config<'a> {
+    pub query: &'a str,
+    pub filename: &'a str
+}
+
+impl<'a> Config<'a> {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = &args[1][..];
+        let filename = &args[2][..];
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let _contents = fs::read_to_string(config.filename)?;
+    Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    //vec![]
+    Vec::new()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+use minigrep;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,38 +10,8 @@ fn main() {
         process::exit(1);
     });
 
-    println!("query {}", config.query);
-    println!("filename {}", config.filename);
-
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Error running search: {}", e);
         process::exit(1);
     };
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let _contents = fs::read_to_string(config.filename)?;
-        //.unwrap_or_else(|err| {
-        //return Err("Something went wrong reading the file");
-        //return "Something went wrong reading the file";
-    //});
-        //.expect("Something went wrong reading the file");
-    //println!("contents {}", contents);
-    Ok(())
-}
-
-struct Config<'a> {
-    query: &'a str,
-    filename: &'a str
-}
-
-impl<'a> Config<'a> {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = &args[1][..];
-        let filename = &args[2][..];
-        Ok(Config { query, filename })
-    }
 }
